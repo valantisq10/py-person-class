0218c7e5089ddbd6b162ae221f50711a04021f9f
Valantis Lempidakis <balantislempidakis@gmail.com>
2024-12-23 21:52:54 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,34 @@
 class Person:
-    # write your code here
-    pass
+    """ Class attribute to store instances by their name"""
+    people = {}
+
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        # Add the instance to the class-level dictionary
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people):
+    """
+    Converts a list of dictionaries into a list of Person instances,
+    linking wife/husband attributes to Person instances.
+    """
+    """ Clear the class attribute to avoid conflicts with previous data"""
+    Person.people.clear()
+
+    """ Create all Person instances"""
+    person_instances = [
+        Person(person["name"], person["age"])
+        for person in people
+    ]
+
+    """ Link wife/husband attributes """
+    for person in people:
+        person_instance = Person.people[person["name"]]
+        if "wife" in person and person["wife"] is not None:
+            person_instance.wife = Person.people[person["wife"]]
+        if "husband" in person and person["husband"] is not None:
+            person_instance.husband = Person.people[person["husband"]]
+
+    return person_instances
