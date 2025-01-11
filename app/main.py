85a043beaa4717117b6918b04acbb08bae50a551
valantisq10 <balantislempidakis@gmail.com>
2025-01-11 21:20:09 +0000

    modified:   app/main.py

--- app/main.py
+++ app/main.py
@@ -1,37 +1,37 @@
-from typing import List
-
-
 class Person:
-    """ Class attribute to store instances by their name"""
+    # Class attribute to store instances by their name
     people = {}
 
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        # Add the instance to the class-level dictionary
+
+        # Add the instance to the people dictionary
         Person.people[name] = self
 
 
-def create_person_list(people: List) -> List[Person]:
-    """
-    Converts a list of dictionaries into a list of Person instances,
-    linking wife/husband attributes to Person instances.
-    """
-    """ Clear the class attribute to avoid conflicts with previous data"""
-    Person.people.clear()
+def create_person_list(people: list) -> list:
+    # First pass: Create all Person instances
+    person_instances = []
+    for person_dict in people:
+        name = person_dict["name"]
+        age = person_dict["age"]
+        person_instances.append(Person(name, age))
 
-    """ Create all Person instances"""
-    person_instances = [
-        Person(person["name"], person["age"])
-        for person in people
-    ]
-
-    """ Link wife/husband attributes """
-    for person in people:
-        person_instance = Person.people[person["name"]]
-        if "wife" in person and person["wife"] is not None:
-            person_instance.wife = Person.people[person["wife"]]
-        if "husband" in person and person["husband"] is not None:
-            person_instance.husband = Person.people[person["husband"]]
+    # Second pass: Link spouses
+    for person_dict in people:
+        person = Person.people[person_dict["name"]]
+        spouse_key = "wife" if "wife" in person_dict else "husband"
+        spouse_name = person_dict.get(spouse_key)
+        if spouse_name:
+            spouse_instance = Person.people.get(spouse_name)
+            if spouse_key == "wife":
+                person.wife = spouse_instance
+            elif spouse_key == "husband":
+                person.husband = spouse_instance
 
     return person_instances
+
+
+def clear_people() -> None:
+    Person.people.clear()
